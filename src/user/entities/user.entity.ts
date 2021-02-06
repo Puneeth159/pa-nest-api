33cb5e455ec8b0d4b0b432c518767fc4b49f8b35
Puneeth159 <60018781+Puneeth159@users.noreply.github.com>
2021-02-05 21:55:58 -0600

    Commit

---
+++ src/user/entities/user.entity.ts
@@ -0,0 +1,18 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity('User')
+
+export class User {
+    @PrimaryGeneratedColumn()
+    id:number;
+
+    @Column({ 
+        type: 'varchar' 
+    }) 
+    email: string; 
+
+    @Column({ 
+        type: 'varchar'
+    }) 
+    password: string;
+}
---
+++ src/user/user.service.ts
@@ -0,0 +1,41 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from './entities/user.entity';
+import { UpdateResult, DeleteResult } from  'typeorm';
+import { CreateUserDto } from './dto/create-user.dto';
+// import { UpdateUserDto } from './dto/update-user.dto';
+
+@Injectable()
+
+export class UserService {
+  constructor(
+    @InjectRepository(User)
+    private readonly userRepository: Repository<User>
+  ){}
+
+  // create(createUserDto: CreateUserDto) {
+  //   return 'This action adds a new user';
+  // }
+
+  async  findAll(): Promise<User[]> {
+    return await this.userRepository.find();
+}
+
+ async findById(id: number): Promise<User> {
+  return await this.userRepository.findOne(id);
+  
+ }
+
+ async update(user: User): Promise<UpdateResult> {
+  return await this.userRepository.update(user.id, user);
+}
+
+async delete(id): Promise<DeleteResult> {
+  return await this.userRepository.delete(id);
+}
+
+async  create(user: User): Promise<User> {
+  return await this.userRepository.save(user);
+}
+}
